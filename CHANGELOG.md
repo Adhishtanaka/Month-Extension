e2d6863f7c79beb63dcb7cdb809dfb576a4517f7
Adhishtanaka Kulasooriya <kulasoooriyaa@gmail.com>
2024-12-17 20:49:01 +0530

    update version

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -2,6 +2,11 @@
 
 All notable changes to the "Month" extension will be documented in this file. The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/) and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0.html).
 
+## [0.0.6] - 2024-12-17
+
+### Fixed
+- Addressed bug in getLocalToday() due to incorrect UTC date format.
+
 ## [0.0.5] - 2024-12-16
 
 ### Fixed
--- src/util.ts
+++ src/util.ts
@@ -9,9 +9,7 @@ export function debounce(func: Function, delay: number = 5000): Function {
 }
 
 export function getLocalToday(): string {
-  const utcNow = new Date();
-  const timezoneOffset = utcNow.getTimezoneOffset();
-  const now = new Date(utcNow.getTime() - (timezoneOffset * 60000));
+  const now = new Date();
   const year = now.getFullYear();
   const month = String(now.getMonth() + 1).padStart(2, "0"); 
   const day = String(now.getDate()).padStart(2, "0");
